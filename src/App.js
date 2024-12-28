8ac67a674296b60d96df646c31f54f9eadae5ae2
Karthiknayak490 <157905112+Karthiknayak490@users.noreply.github.com>
2024-12-28 08:16:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h3>my app</h3>
+      <p>my app works</p>
+    </>
+  );
+}
+
+export default App;
